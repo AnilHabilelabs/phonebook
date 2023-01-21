fd8950d4e1987411dfa2066bb0978e4e3f31a995
Anil Kumar <anil.kumar@habilelabs.io>
2023-01-21 17:50:16 +0530

    all functionality added

--- js/main.js
+++ js/main.js
@@ -1,49 +1,61 @@
 let table = document.querySelector(".contactTable");
 
-// function validation(e){
-//   for(let i =0;i<e.target[i]-2;i++){
-//     if(e.target[i].value == ""){
-//       return false;
-//     }
-//   }
-//   saveContact(e);
-// }
+function validation(e) {
+  console.log(e);
+  for (let i = 0; i < e.target.length - 2; i++) {
+    if (e.target[i].value == "") {
+      alert(e.target[i].name + " cannot be empty");
+      return false;
+    }
+  }
+  saveContact(e);
+}
 
 function saveContact(e) {
   e.preventDefault();
   let fullname = document.querySelector("#fullname").value.trim();
   let telephone = document.querySelector("#telephone").value.trim();
   let email = document.querySelector("#email").value.trim();
+  let btn = document.querySelector(".savebtn");
+
   // console.log(fullname, telephone, email);
   let contacts = { fullname: fullname, telephone: telephone, email: email };
   let contact_string = JSON.stringify(contacts);
-  localStorage.setItem(email, contact_string);
+
+  if (btn.innerHTML == "Save") {
+    localStorage.setItem(localStorage.length + 1, contact_string);
+  } else {
+    localStorage.setItem(btn.getAttribute("data_num"), contact_string);
+  }
 
   let element = ` <tr class="td_tr">
     <td class="sr">${localStorage.length}</td>
     <td>${fullname}</td>
     <td>${telephone}</td>
     <td>${email}</td>
-    <td> <i class="fa fa-pencil-square-o" aria-hidden="true"> </i> <i class="fa fa-trash-o deleteicon"></i></td>
-    <td></td>
+    <td> <i class="fa fa-pencil-square-o editicon" aria-hidden="true">  </i>
+    <i class="fa fa-trash-o deleteicon"> </i>
+    </td>
     
   </tr>`;
   table.innerHTML += element;
   noContact();
+  location.reload();
 }
 
 let count;
 let storage = localStorage;
 
-for (count = 0; count < storage.length; count++) {
-  let dataFetch = JSON.parse(localStorage.getItem(storage.key(count)));
-
+for (count = 1; count <= storage.length; count++) {
+  let dataFetch = JSON.parse(localStorage.getItem(count));
   table.innerHTML += ` <tr class="td_tr">
-    <td class="sr">${count + 1}</td>
+    <td class="sr">${count}</td>
     <td>${dataFetch.fullname}</td>
     <td>${dataFetch.telephone}</td>
     <td>${dataFetch.email}</td>
-    <td> <i class="fa fa-pencil-square-o editicon" aria-hidden="true">  <i class="fa fa-trash-o deleteicon"></i></td>
+    <td> <i class="fa fa-pencil-square-o editicon" aria-hidden="true">  </i>
+    <i class="fa fa-trash-o deleteicon"> </i>
+    </td>
   </tr>`;
 }
 
@@ -59,25 +71,42 @@ function deleteContact() {
   let deleteIcon = document.querySelectorAll(".deleteicon");
   deleteIcon.forEach((element) => {
     element.onclick = function () {
-      console.log("hello");
+      // console.log("hello");
       let delTr = this.parentElement.parentElement;
-      console.log(delTr);
-
+      // console.log(delTr);
+      let titleName = delTr.querySelectorAll("td")[0].innerText.trim();
+      localStorage.removeItem(titleName);
+      location.reload();
     };
   });
 }
 
-
-
-function editContact(){
+function editContact() {
   let editIcons = document.querySelectorAll(".editicon");
   editIcons.forEach((element) => {
     element.onclick = function () {
-      console.log("hii");
+      // console.log(element);
       let editTr = this.parentElement.parentElement;
-      console.log(editTr);
+      // console.log(editTr);
+      let titleName = editTr.querySelectorAll("td")[0].innerText.trim();
+      let contact = localStorage.getItem(titleName);
+      let contact_obj = JSON.parse(contact);
+      console.log(contact_obj);
+      document.getElementById("fullname").value = contact_obj.fullname;
+      document.getElementById("telephone").value = contact_obj.telephone;
+      document.getElementById("email").value = contact_obj.email;
+      console.log(document.getElementsByClassName("savebtn"));
+      document.getElementsByClassName("savebtn")[0].innerText = "Update";
+      document
+        .getElementsByClassName("savebtn")[0]
+        .setAttribute("data_num", titleName);
     };
   });
 }
 editContact();
 deleteContact();
+
+function cancelbtn() {
+  document.getElementsByClassName("formDiv").reset();
+}
+
